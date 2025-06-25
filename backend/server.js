7e905c0b62a065fb967eb4e7874542a0e1887734
Beladroid <princecaleb040@gmail.com>
2025-06-25 13:52:07 +0100

    moongooses

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,7 @@ const upload = multer({ storage });
 
 // ✅ MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
